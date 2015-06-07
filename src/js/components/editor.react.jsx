eddf18beeb0514df49e3e3d3f460ba4c3c64b437
leozdgao <gzdleo0816@gmail.com>
2015-06-08 00:15:13 +0800

    init editor

--- src/js/components/editor.react.jsx
+++ src/js/components/editor.react.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default React.createClass({
   render: function() {
-    return <div></div>
+    return (
+      <div className="md-panel">
+        <div className="md-toolbar"></div>
+        <div className="md-editor">
+          <textarea></textarea>
+        </div>
+        <div className="md-preview"></div>
+      </div>
+      );
   }
 });
