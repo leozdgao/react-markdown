b01777ccf2141b8aa1051280f5910fa8a30dd2e5
leozdgao <gzdleo0816@gmail.com>
2015-06-18 17:53:00 +0800

    add toolbar function

--- src/js/components/editor.react.jsx
+++ src/js/components/editor.react.jsx
@@ -19,17 +19,17 @@ export default React.createClass({
     return (
       <div className="md-panel">
         <ul className="md-toolbar">
-          <li className="tb-btn"><a title="加粗"><i className="fa fa-bold"></i></a></li> {/* bold */}
-          <li className="tb-btn"><a title="斜体"><i className="fa fa-italic"></i></a></li> {/* italic */}
+          <li className="tb-btn"><a title="加粗" onClick={this._boldText}><i className="fa fa-bold"></i></a></li> {/* bold */}
+          <li className="tb-btn"><a title="斜体" onClick={this._italicText}><i className="fa fa-italic"></i></a></li> {/* italic */}
           <li className="tb-btn spliter"></li>
-          <li className="tb-btn"><a title="链接"><i className="fa fa-link"></i></a></li> {/* link */}
-          <li className="tb-btn"><a title="引用"><i className="fa fa-outdent"></i></a></li> {/* blockquote */}
-          <li className="tb-btn"><a title="代码段"><i className="fa fa-code"></i></a></li> {/* code */}
-          <li className="tb-btn"><a title="图片"><i className="fa fa-picture-o"></i></a></li> {/* picture-o */}
+          <li className="tb-btn"><a title="链接" onClick={this._linkText}><i className="fa fa-link"></i></a></li> {/* link */}
+          <li className="tb-btn"><a title="引用" onClick={this._blockquoteText}><i className="fa fa-outdent"></i></a></li> {/* blockquote */}
+          <li className="tb-btn"><a title="代码段" onClick={this._codeText}><i className="fa fa-code"></i></a></li> {/* code */}
+          <li className="tb-btn"><a title="图片" onClick={this._pictureText}><i className="fa fa-picture-o"></i></a></li> {/* picture-o */}
           <li className="tb-btn spliter"></li>
-          <li className="tb-btn"><a title="有序列表"><i className="fa fa-list-ol"></i></a></li> {/* list-ol */}
-          <li className="tb-btn"><a title="无序列表"><i className="fa fa-list-ul"></i></a></li> {/* list-ul */}
-          <li className="tb-btn"><a title="标题"><i className="fa fa-header"></i></a></li> {/* header */}
+          <li className="tb-btn"><a title="有序列表" onClick={this._listOlText}><i className="fa fa-list-ol"></i></a></li> {/* list-ol */}
+          <li className="tb-btn"><a title="无序列表" onClick={this._listUlText}><i className="fa fa-list-ul"></i></a></li> {/* list-ul */}
+          <li className="tb-btn"><a title="标题" onClick={this._headerText}><i className="fa fa-header"></i></a></li> {/* header */}
 
           <li className="tb-btn pull-right"><a title="预览模式"><i className="fa fa-eye"></i></a></li> { /* preview mode */ }
           <li className="tb-btn pull-right"><a title="分屏模式"><i className="fa fa-columns"></i></a></li> { /* split mode */ }
@@ -68,5 +68,42 @@ export default React.createClass({
   _dragstart: function(e) {
     e.preventDefault();
     e.stopPropergation();
+  },
+  _preInputText: function(text, preStart, preEnd) {
+    let start = this.textControl.selectionStart,
+        end = this.textControl.selectionEnd,
+        origin = this.textControl.value
+
+    this.textControl.value = origin.slice(0, start) + text + origin.slice(end);
+    // pre-select
+    this.textControl.setSelectionRange(start + preStart, start + preEnd);
+    this.setState({ content: { __html: marked(this.textControl.value) } });
+  },
+  _boldText: function() {
+    this._preInputText("**加粗文字**", 2, 6);
+  },
+  _italicText: function() {
+    this._preInputText("_斜体文字_", 1, 5);
+  },
+  _linkText: function() {
+    this._preInputText("[链接文本](www.yourlink.com)", 1, 5);
+  },
+  _blockquoteText: function() {
+    this._preInputText("> 引用", 2, 4);
+  },
+  _codeText: function() {
+    this._preInputText("```\ncode block\n```", 4, 14);
+  },
+  _pictureText: function() {
+    this._preInputText("![alt](www.yourlink.com)", 2, 5);
+  },
+  _listUlText: function() {
+    this._preInputText("- 无序列表项0\n- 无序列表项1", 2, 8);
+  },
+  _listOlText: function() {
+    this._preInputText("1. 有序列表项0\n2. 有序列表项1", 3, 9);
+  },
+  _headerText: function() {
+    this._preInputText("## 标题", 3, 5);
   }
 });
