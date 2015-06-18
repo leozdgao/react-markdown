c0dbf4538a63223673854d81f761da0eb6f637c8
leozdgao <gzdleo0816@gmail.com>
2015-06-18 11:29:03 +0800

    resizer

--- src/js/components/editor.react.jsx
+++ src/js/components/editor.react.jsx
@@ -2,23 +2,29 @@ import React from 'react';
 import marked from 'marked';
 
 export default React.createClass({
-  getInitialState: function() {
-    return { __html: '' };
+  getInitialState: function() { console.log('state');
+    return {
+      content: { __html: '' },
+      editorHeight: 540
+    };
   },
-  componentDidMount: function() {
+  componentDidMount: function() { console.log('mount');
     // check dom node
     this.textControl = React.findDOMNode(this.refs.editor);
     this.previewControl = React.findDOMNode(this.refs.preview);
+    this.resizer = React.findDOMNode(this.refs.resizer);
+    this.minEditorHeight = this.state.editorHeight;
   },
   render: function() {
     return (
       <div className="md-panel">
         <div className="md-toolbar"></div>
         <div className="md-editor">
-          <textarea ref="editor" onChange={this._onChange}></textarea>
+          <textarea ref="editor" onChange={this._onChange} style={{height: this.state.editorHeight + 'px'}}></textarea>
         </div>
-        <div ref="preview" className="md-preview markdown" dangerouslySetInnerHTML={this.state}></div>
+        <div ref="preview" className="md-preview markdown" dangerouslySetInnerHTML={this.state.content}></div>
         <div className="md-spliter"></div>
+        <div ref="resizer" className="md-resizer" onMouseDown={this._mousedown}></div>
       </div>
     );
   },
@@ -29,5 +35,16 @@ export default React.createClass({
       let input = this.textControl.value;
       this.setState({ __html: marked(input) });
     }, 300);
+  },
+  _mousedown: function(e) {
+    document.addEventListener('mousemove', this._mousemove);
+    document.addEventListener('mouseup', this._mouseup);
+  },
+  _mouseup: function(e) {
+    document.removeEventListener('mousemove', this._mousemove);
+  },
+  _mousemove: function(e) {
+    let h = e.clientY - this.textControl.getBoundingClientRect().top; console.log(e.screenY); console.log(this.textControl.offsetTop);
+    this.setState({ editorHeight: Math.max(h, this.minEditorHeight) });
   }
 });
