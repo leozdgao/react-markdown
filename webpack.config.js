44a3344214efed7962ef9f8a1a97f7bf6a543f48
leozdgao <gzdleo0816@gmail.com>
2015-08-05 14:26:59 +0800

    ready to refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var path = require('path');
-var webpack = require('webpack');
-var glob = require('glob');
+var path = require('path')
+var webpack = require('webpack')
+var glob = require('glob')
 
-function getEntries() {
-    var entries = {};
-    glob.sync(__dirname + '/src/**/*.main.js').forEach(function (name) {
-        var n = name.match(/\/([^\/]+?).main.js/)[1];
-        entries[n] = name;
-    });
-    return entries;
+function getEntries () {
+  var entries = {}
+  glob.sync(__dirname + '/src/**/*.main.js').forEach(function (name) {
+    var n = name.match(/\/([^\/]+?).main.js/)[1]
+    entries[n] = name
+  })
+  return entries
 }
 
 module.exports = {
-    entry: getEntries(),
-    output: {
-        path: path.join(__dirname, './dist'),
-        filename: '[name].min.js',
-        sourceMapFilename: '[file].map'
-    },
-    externals: {
-        'react': 'React',
-        'marked': 'marked',
-        'highlight': 'hljs'
-    },
-    devtool: 'source-map',
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    }
+  entry: getEntries(),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: '[name].min.js',
+    sourceMapFilename: '[file].map'
+  },
+  externals: {
+    'react': 'React',
+    'marked': 'marked',
+    'highlight': 'hljs'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 }
